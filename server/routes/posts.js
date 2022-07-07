0e47572595528531d7eb518fab47d43a0111becb
Ketaki-k-jadhav <ketakijadhav2612@gmail.com>
2022-07-07 13:38:34 +0530

    Added route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPosts } from '../controllers/posts.js';
 
 const router=express.Router();
 
 // localhost:5000/posts/ and not localhost:5000/ =>app.use('/posts',postRoutes); in index.js
 router.get('/', getPosts);
-
+router.post('/',createPosts);
 export default router;
