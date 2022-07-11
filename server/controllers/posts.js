345f445c80789b97a22cf77c8158175de45c2c83
Ketaki-k-jadhav <ketakijadhav2612@gmail.com>
2022-07-11 16:28:55 +0530

    some changes

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,4 @@
-import PostMessage from '../models/postMessage,js';
+import PostMessage from '../models/postMessage.js';
 
 //retrieving data from database takes some time so this is an asynchronous function
 export const getPosts=async(req,res) => {
